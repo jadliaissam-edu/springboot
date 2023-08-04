f723204a27862b5b8e3136bccb293e8b7abf3377
jassam <jadliaissam@gmail.com>
2023-08-04 01:01:33 +0100

    add upload code

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,13 +1,40 @@
 package com.example.demo.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController("/")
 public class HelloController {
+    private static final String UPLOADED_FOLDER = "uploaded/";
+
 
     @GetMapping("")
     public String hello(){
         return "Hello World Spring Boot !";
     }   
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> singleFileUpload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return new ResponseEntity<>("please select a file!", HttpStatus.OK);
+        }
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+             return new ResponseEntity<>("Successfully uploaded the file", HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
